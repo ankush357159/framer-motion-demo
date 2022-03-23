d6b389592c6464b385836f5669f3ad0c149fe129
Ankush <ankush357159@gmail.com>
2022-03-23 17:48:31 +0530

    basic framer-motion

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration:0.3,
+      yoyo:Infinity
+    }
+  },
+};
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
+    <motion.div
+      className='home container'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
+    >
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to='/base'>
+        <motion.button variants={buttonVariants} whileHover='hover'>
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
